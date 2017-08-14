8de10c58be9188d2f685d7f623291358c48863e5
Sergio Huerta <sergio.huerta@caliente.com.mx>
2017-08-14 16:02:16 -0700

    First commit

---
+++ Booleanos.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var b1:Bool = true
+
+var b2:Bool = false
+
+
+//Operadores de asignacion
+
+var x = 20
+var y = 10
+
+var comparacion = x==y
+
+comparacion
+
+
+//concatenar
+
+let const1:String="ABCDEF"
+let const2:String="0123456789"
+
+var concatenacion:String = const1 + const2
+
+let char1:Character = "A"
+let char2:Character = "B"
+
+var union:String = String(char1) + String(char2)
