a8748ca7362dd1334463cafb391929f58e3bc157
Sergio Huerta <sergio.huerta@caliente.com.mx>
2017-08-14 18:56:56 -0700

    Google Maps con CocoaPods

---
+++ Mercedes Benz/Pods/Target Support Files/Pods-Mercedes Benz/Pods-Mercedes Benz-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mercedes_Benz : NSObject
+@end
+@implementation PodsDummy_Pods_Mercedes_Benz
+@end
---
+++ Mercedes Benz/Pods/Target Support Files/Pods-Mercedes BenzUITests/Pods-Mercedes BenzUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mercedes_BenzUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Mercedes_BenzUITests
+@end
