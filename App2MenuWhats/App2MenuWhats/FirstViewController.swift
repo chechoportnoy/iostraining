f26f6e70d7a07f7ab0d3caa2d3b171c72acf6192
Sergio Huerta <sergio.huerta@caliente.com.mx>
2017-08-15 11:06:57 -0700

    Clase 2 - Menu whats

---
+++ App2MenuWhats/App2MenuWhats/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  App2MenuWhats
+//
+//  Created by Sergio Huerta on 8/15/17.
+//  Copyright © 2017 Sergio Huerta. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class FirstViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        os_log("ViewDidLoad 1")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ App2MenuWhats/App2MenuWhats/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  App2MenuWhats
+//
+//  Created by Sergio Huerta on 8/15/17.
+//  Copyright © 2017 Sergio Huerta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
