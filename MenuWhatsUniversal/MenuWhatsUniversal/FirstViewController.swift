fe95eb90259ddcf73b92872ef16b6e3718a5c2d5
Sergio Huerta <sergio.huerta@caliente.com.mx>
2017-08-15 13:21:34 -0700

    Agregue un Alert en la primera vista

--- MenuWhatsUniversal/MenuWhatsUniversal/FirstViewController.swift
+++ MenuWhatsUniversal/MenuWhatsUniversal/FirstViewController.swift
@@ -20,6 +20,24 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func mostrarMensaje(_ sender: Any) {
+        /*let alert = UIAlertController(title: "Alert", message: "Vista 1", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)*/
+        let alertController = UIAlertController(title: "Alerta", message: "Esta es la vista 1.", preferredStyle: UIAlertControllerStyle.alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { (result : UIAlertAction) -> Void in
+            print("Cancel")
+        }
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+            print("OK")
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
 
 }
 
